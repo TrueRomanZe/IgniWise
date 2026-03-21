a151336b914d2bfb1a0c184df96cb984736055da
S.Romera <s.romera92@gmail.com>
2026-03-21 07:51:28 +0100

    Add docstring and update __all__ in init.py
    Added module docstring and updated exports.

---
+++ src/data_processing/init.py
@@ -0,0 +1,11 @@
+"""
+IgniWise - Data Processing
+Módulo de procesamiento de datos y feature engineering
+"""
+
+from .calculate_fwi import calculate_fwi_components, interpret_fwi
+
+__all__ = [
+    'calculate_fwi_components',
+    'interpret_fwi'
+]
