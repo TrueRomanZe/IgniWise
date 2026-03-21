c9549e379582ae258976fbe578653dc2df1c5b9c
S.Romera <s.romera92@gmail.com>
2026-03-21 07:48:30 +0100

    Add module docstring and metadata to init.py

---
+++ src/init.py
@@ -0,0 +1,11 @@
+"""
+IgniWise - Sistema de Predicción de Ventanas de Quema Prescrita
+Copyright © 2026 Sergio Romera Martínez
+Licencia: MIT
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Sergio Romera Martínez'
+__email__ = 's.romera92@gmail.com'
+
+# Paquete principal de IgniWise
