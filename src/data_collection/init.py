8b416802d64c4b01d2f41541e5d3846a15a5a110
S.Romera <s.romera92@gmail.com>
2026-03-21 07:50:43 +0100

    Add docstring to init.py
    Add module docstring for data collection

---
+++ src/data_collection/init.py
@@ -0,0 +1,10 @@
+"""
+IgniWise - Data Collection
+Módulo de recolección de datos desde fuentes externas
+"""
+
+# Importar funciones principales si es necesario
+# from .download_miteco import descargar_datos_miteco
+# from .download_geodata import descargar_geodata
+
+__all__ = []
