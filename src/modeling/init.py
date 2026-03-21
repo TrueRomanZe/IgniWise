ee9499305d943056cd80b3256e2787461eb0b827
S.Romera <s.romera92@gmail.com>
2026-03-21 07:51:58 +0100

    Add docstring and import comments to init.py
    Add module docstring and comments for imports

---
+++ src/modeling/init.py
@@ -0,0 +1,10 @@
+"""
+IgniWise - Modeling
+Módulo de entrenamiento y predicción con Machine Learning
+"""
+
+# Funciones disponibles para importar
+# from .train_model import main as train_model
+# from .predict import main as predict
+
+__all__ = []
